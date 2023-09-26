0836882addb2d29ac313d75efad40e95c4ea23dc
mandar_09 <64251657+mandarborkar2304@users.noreply.github.com>
2023-09-26 12:17:31 +0530

    Add files via upload

---
+++ nltk_utils.py
@@ -0,0 +1,18 @@
+import nltk
+nltk.download('punkt')
+from nltk.stem.porter import PorterStemmer
+stemmer = PorterStemmer()
+
+def tokenize(sentence):
+    return nltk.word_tokenize(sentence)
+
+def stem(word):
+    return stemmer.stem(word.lower())
+
+def bag_of_words(tokenized_sentence, allwords):
+    pass
+
+a = "How long does Summer"
+print(a)
+a= tokenize(a)
+print(a)
